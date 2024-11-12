53ca8c06c13a11963e5cb0edfa6439a5e13be27a
João Marcos Simon Paulini <jmsp@JM-MacBook-Pro.local>
2024-11-11 21:42:19 -0300

    0

--- script.js
+++ script.js
@@ -11,21 +11,10 @@ document.querySelector("form").addEventListener("submit", function(event) {
   
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
-  
-  // Validação do usuário e senha
-  if (username === "felipe" && password === "tigrinhosimulator123") {
-    setCookie("authToken", "authenticated-felipe", 10); // Cookie válido por 10 minutos
-    window.location.href = "ola_felipe.html";
-  } else if (username === "nicolas" && password === "jogodogatinho") {
-    setCookie("authToken", "authenticated-nicolas", 10); // Cookie válido por 10 minutos
-    window.location.href = "ola_nicolas.html";
-  } else if (username === "victor" && password === "carton") {
-    setCookie("authToken", "authenticated-victor", 10); // Cookie válido por 10 minutos
-    window.location.href = "ola_victor.html";  
-  } else if (username === "diego" && password === "amigos") {
-    setCookie("authToken", "authenticated-diego", 10); // Cookie válido por 10 minutos
-    window.location.href = "ola_diego.html";      
+
+  if (username === "victor" && password === "aposta") {
+    setCookie("authToken", "authenticated-victor", 10);
+    window.location.href = "ola_victor.html";
   } else {
     alert("Usuário ou senha incorretos!");
   }
-});
